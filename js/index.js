e3cb37967192a60f23e182bc62292c673e259b4b
Slogllykop <slogllykop07@gmail.com>
2022-11-24 14:14:16 +0530

    fixed login button usability

--- js/index.js
+++ js/index.js
@@ -9,17 +9,23 @@ const loginUsername = document.querySelector('.inputted_username');
 const loginPassword = document.querySelector('.inputted_password');
 
 const loginButton = document.querySelector('.login_submit');
+const loginForm = document.querySelector('.login_form');
 
-loginPage.addEventListener("keypress", function() {
-    if (loginUsername.value.length > 0 && loginPassword.value.length >= 4) {
+
+function checkLength() {
+    if (loginUsername.value.length !== 0 && loginPassword.value.length >= 4) {
+        console.log("user:", loginUsername.value.length, "pass:", loginPassword.value.length);
         loginButton.disabled = false;
         loginButton.style.cursor = "pointer";
         loginButton.style.backgroundColor = "rgb(0, 149, 246)";
     } else {
         loginButton.disabled = true;
         loginButton.style.cursor = "not-allowed";
+        loginButton.style.backgroundColor = "rgb(178, 223, 252)";
     }
-})
+}
+
+loginForm.addEventListener("keyup", checkLength)
 
 const login = () => {
     console.log(loginUsername.value.length, loginPassword.value.length);
